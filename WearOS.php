a5dbe53ad5df835972b785595dafb02a695c92a3
Floofer-droid <70661432+Floofer-droid@users.noreply.github.com>
2021-09-24 11:42:00 -0400

    Create WearOS.php
    lab5 step2.5

---
+++ WearOS.php
@@ -0,0 +1,10 @@
+<!DOCTYPE php> 
+<html> 
+    <head> 
+        <title>php page</title> 
+    </head> 
+    <body> 
+        <p>your user agent is <?= $_POST["HTTP_USER_AGENT"]?></p>
+    </body> 
+
+</html> 
